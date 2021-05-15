8f136a15646cb8dba43bddda8025bc76cada3598
Chris Withers <chris@simplistix.co.uk>
2021-05-15 09:25:33 +0100

    Use the database instead of Reading Uni's API.

--- predict.py
+++ predict.py
@@ -1,10 +1,12 @@
 from argparse import ArgumentParser
-from csv import reader as csv_reader
 from datetime import datetime, date, timedelta, time
-from io import StringIO
 
 from dateutil.parser import parse as parse_date
-import requests
+from sqlalchemy import func, select
+from sqlalchemy.orm import Session
+
+from data import connect
+from schema import Observation
 
 ZONE_TO_MM_PER_MIN = {
     'house': 0.097944482495229,
@@ -21,48 +23,21 @@ def last_sunday():
     return datetime.combine(dt, time(0, 0))
 
 
-def reader(content):
-    r = csv_reader(StringIO(content))
-    next(r)  # header
-    try:
-        next(r)  # units
-    except StopIteration:
-        # empty?
-        return
-    for dt, value in r:
-        yield parse_date(dt), float(value)
-
-
-def download(series, var, start, end):
-    response = requests.get(
-        f'https://metdata.reading.ac.uk/ext/dataset/{series}/get_data',
-        params=dict(
-            token='public',
-            start_date=start.strftime('%Y-%m-%d-%H:%M:%S'),
-            end_date=end.strftime('%Y-%m-%d-%H:%M:%S'),
-            var=var,
-            data_format='csv'
-        ))
-    response.raise_for_status()
-    return reader(response.content.decode())
-
-
 def reading_uni_rainfall(start, end):
-    rain = 0.0
-    earliest = None
-    for start, value in download('climat0900', 'Rain_accum_0909', start+timedelta(days=1), end):
-        if earliest is None:
-            earliest = start-timedelta(days=1)
-        rain += value
-    start += timedelta(hours=1)
-    for start, value in download('1hour_Level2', 'Rain', start, end):
-        if earliest is None:
-            earliest = start-timedelta(hours=1)
-        rain += value
-
-    latest = start
-
-    print(f'Between {earliest} and {latest}, {rain:.1f}mm of rain has fallen.')
+    session = Session(connect(), future=True)
+    rain, earliest, latest = session.execute(
+        select(
+            func.sum(Observation.value),
+            func.min(Observation.timestamp),
+            func.max(Observation.timestamp),
+        ).where(
+            Observation.dataset == '1hour_Level2',
+            Observation.variable == 'Rain',
+            Observation.timestamp.between(start, end),
+        )
+    ).one()
+    print(f'Between {earliest:%a %d %b %H:%M} and {latest:%a %d %b %H:%M}, '
+          f'{rain:.1f}mm of rain has fallen.')
     return rain
 
 
