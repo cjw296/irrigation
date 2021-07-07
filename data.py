0f7bc8960e14d4570c4c3ec8b6fba6256b3b0921
Chris Withers <chris@simplistix.co.uk>
2021-07-07 09:21:15 +0100

    factor out and add start/end

--- data.py
+++ data.py
@@ -1,7 +1,11 @@
-from pandas import read_sql
-from sqlalchemy import create_engine
+from datetime import datetime
+from typing import Sequence
+
+from pandas import read_sql, DataFrame
+from sqlalchemy import create_engine, select
 
 from config import config
+from schema import Observation
 
 
 def db_url():
@@ -13,33 +17,36 @@ def connect():
     return create_engine(db_url(), echo=False)
 
 
-def load_daily():
-    return read_sql("select * from observation where dataset='climat0900'", db_url()).pivot(
+def load(dataset: str, start: datetime = None) -> DataFrame:
+    query = select(Observation).where(Observation.dataset == dataset)
+    if start:
+        query = query.where(Observation.timestamp >= start)
+    return read_sql(query, db_url()).pivot(
         index='timestamp', columns='variable', values='value'
     )
 
 
-def load_hourly():
-    return read_sql("select * from observation where dataset='1hour_Level2'", db_url()).pivot(
-        index='timestamp', columns='variable', values='value'
-    )
+def load_daily(start: datetime = None) -> DataFrame:
+    return load('climat0900', start)
 
 
-def load_hourly_maxmin():
-    return read_sql("select * from observation where dataset='1hour_Level2_maxmin'", db_url()).pivot(
-        index='timestamp', columns='variable', values='value'
-    )
+def load_hourly(start: datetime = None) -> DataFrame:
+    return load('1hour_Level2', start)
 
 
-def daily_from_hourly(hourly, variables):
+def load_hourly_maxmin(start: datetime = None) -> DataFrame:
+    return load('1hour_Level2_maxmin', start)
+
+
+def daily_from_hourly(hourly: DataFrame, variables: Sequence[str]) -> DataFrame:
     origin = hourly.index.min().replace(hour=9)
     resampler = hourly[variables].resample('D', origin=origin, label='right')
     return resampler.agg({v: "max" if v.endswith('max') else 'min' for v in variables})
 
 
-def combined_data():
-    daily = load_daily()
-    hourly = load_hourly_maxmin()
+def combined_data(start: datetime = None) -> DataFrame:
+    daily = load_daily(start)
+    hourly = load_hourly_maxmin(start)
     daily[['P_max', 'P_min']] = daily_from_hourly(hourly, ['P_max', 'P_min']) * 0.1  # hPa to kPa
     daily[['RH_max', 'RH_min']] = daily_from_hourly(hourly, ['RH_max', 'RH_min'])
     return daily
