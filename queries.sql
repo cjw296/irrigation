0d9a0c7a21af554c99b03128b0af771d9206655b
Chris Withers <chris@simplistix.co.uk>
2023-04-16 16:08:33 +0100

    Add queries for de-duplicating climate_extract_cgi / RR data

--- queries.sql
+++ queries.sql
@@ -3,3 +3,9 @@ SELECT date_trunc('year', timestamp)::date as dt, min(timestamp) as earliest, ma
 
 -- Daily rainfall summed from hourly
 SELECT date_trunc('day', timestamp)::date as dt, max(timestamp), min(timestamp), sum(value)::numeric from observation where dataset='1hour_Level2' and variable='Rain' group by dt order by dt desc limit 20;
+
+-- Deleting duplicate RR
+select * from observation where dataset='climate_extract_cgi' and timestamp in (select timestamp from observation where dataset='climate_extract_cgi' group by 1 having count(*)>1);
+
+-- do this in a transaction!
+delete from observation where id in (select max(id) from observation where dataset='climate_extract_cgi' group by timestamp having count(*)>1);
